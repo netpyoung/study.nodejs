179ab4e7aa33e2650bc37b392384b1802c664239
pyoung@archbang <netpyoung@gmail.com>
2013-05-22 01:21:28 +0900

    초벌updatie

---
+++ pyoung/tmp/main.js
@@ -0,0 +1,7 @@
+// 모듈을 불러올땐 require
+var mymodule = require('./mymodule.js');
+
+console.log('abs(-273) = %d', mymodule.abs(-273));
+console.log('circleArea(3) = %d', mymodule.circleArea(3));
+console.log(mymodule);
+console.log(process.env);
---
+++ pyoung/tmp/mymodule.js
@@ -0,0 +1,7 @@
+exports.abs = function (n) {
+    return (0 < n) ? n : -n;
+};
+
+exports.circleArea = function (r) {
+    return r * r * Math.PI;
+};

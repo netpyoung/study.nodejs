ffce148e272b31ffa7526a47bf1db3f9f6358a56
netpyoung <netpyoung@gmail.com>
2013-05-22 08:19:14 +0900

    updating

--- pyoung/tmp/main.js
+++
@@ -1,7 +0,0 @@
-// 모듈을 불러올땐 require
-var mymodule = require('./mymodule.js');
-
-console.log('abs(-273) = %d', mymodule.abs(-273));
-console.log('circleArea(3) = %d', mymodule.circleArea(3));
-console.log(mymodule);
-console.log(process.env);
--- pyoung/tmp/mymodule.js
+++
@@ -1,7 +0,0 @@
-exports.abs = function (n) {
-    return (0 < n) ? n : -n;
-};
-
-exports.circleArea = function (r) {
-    return r * r * Math.PI;
-};
